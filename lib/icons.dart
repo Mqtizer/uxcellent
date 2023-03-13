fcaa14fe79e33004cb57d50e64e23f4bdc75b932
Sanyam Arya <er.sanyam.arya@gmail.com>
2023-03-13 14:02:32 +0100

    Update icons in lib/icons.dart
    Bulleted Messages:
    - Replaced SizedBox with SvgPicture.asset for Home, Home-active, Template, Template-active, Learning and Learning-active icons
    - Updated colorFilter to ColorFilter.mode(color, BlendMode.srcIn)
    - Updated package to 'uxcellent'

--- lib/icons.dart
+++ lib/icons.dart
@@ -6,88 +6,77 @@ Widget homeIcon({
   Color color = Colors.white,
   double size = 24,
 }) =>
-    SizedBox(
+    SvgPicture.asset(
+      'assets/icons/home.svg',
+      semanticsLabel: 'Home',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      package: 'uxcellent',
       width: size,
       height: size,
-      child: SvgPicture.asset(
-        'assets/icons/home.svg',
-        semanticsLabel: 'Home',
-        colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
-        package: 'uxcellent',
-      ),
     );
 
 Widget homeActiveIcon({
   Color color = Colors.white,
   double size = 24,
 }) =>
-    SizedBox(
+    SvgPicture.asset(
+      'assets/icons/home-active.svg',
+      semanticsLabel: 'Home',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      package: 'uxcellent',
       width: size,
       height: size,
-      child: SvgPicture.asset(
-        'assets/icons/home-active.svg',
-        semanticsLabel: 'Home',
-        colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
-        package: 'uxcellent',
-      ),
+     
     );
 
 Widget templateIcon({
   Color color = Colors.white,
   double size = 24,
 }) =>
-    SizedBox(
+    SvgPicture.asset(
+      'assets/icons/template.svg',
+      semanticsLabel: 'Template',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      package: 'uxcellent',
       width: size,
       height: size,
-      child: SvgPicture.asset(
-        'assets/icons/template.svg',
-        semanticsLabel: 'Template',
-        colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
-        package: 'uxcellent',
-      ),
     );
 
 Widget templateActiveIcon({
   Color color = Colors.white,
   double size = 24,
 }) =>
-    SizedBox(
+    SvgPicture.asset(
+      'assets/icons/template-active.svg',
+      semanticsLabel: 'Template',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      package: 'uxcellent',
       width: size,
       height: size,
-      child: SvgPicture.asset(
-        'assets/icons/template-active.svg',
-        semanticsLabel: 'Template',
-        colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
-        package: 'uxcellent',
-      ),
     );
 
 Widget learningIcon({
   Color color = Colors.white,
   double size = 24,
 }) =>
-    SizedBox(
+    SvgPicture.asset(
+      'assets/icons/learning.svg',
+      semanticsLabel: 'Learning',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      package: 'uxcellent',
       width: size,
       height: size,
-      child: SvgPicture.asset(
-        'assets/icons/learning.svg',
-        semanticsLabel: 'Learning',
-        colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
-        package: 'uxcellent',
-      ),
     );
 
 Widget learningActiveIcon({
   Color color = Colors.white,
   double size = 24,
 }) =>
-    SizedBox(
+    SvgPicture.asset(
+      'assets/icons/learning-active.svg',
+      semanticsLabel: 'Learning',
+      colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
+      package: 'uxcellent',
       width: size,
       height: size,
-      child: SvgPicture.asset(
-        'assets/icons/learning-active.svg',
-        semanticsLabel: 'Learning',
-        colorFilter: ColorFilter.mode(color, BlendMode.srcIn),
-        package: 'uxcellent',
-      ),
     );
